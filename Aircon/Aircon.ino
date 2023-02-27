05dda61c61df129b0ccaf238fbe6a42a8016a96d
Sam Lane <minordeveloper@outlook.com>
2023-02-27 13:33:03 +0000

    Initial kicad and plan

---
+++ Aircon/Aircon.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // Configure pin io
+
+  // D5,6,7,8 are used for the OP amp
+
+  // Setup the I2C ADC
+  // (I presume the usual I2C pins)
+}
+
+void loop() {
+  // Read in the analogue inputs each time
+
+  // Ramp up and down each channel of the OP amp
+
+  // Read the temperature
+
+}
